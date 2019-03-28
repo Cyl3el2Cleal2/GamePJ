54a3fa0a96d3e38c85f93d93b4a3c7b9416462c0
Cyl3el2Cleal2 <natha_sk@hotmail.com>
2019-03-28 20:23:50 +0700

    Canvas 1600 900

--- js/game2.js
+++ js/game2.js
@@ -3,15 +3,14 @@ var timer=10;
 var clock;
 var ctx;
 function createArea() {
-
-    var div = document.getElementById("gameArea")
-    canvas = document.createElement('canvas')
-    canvas.height = div.height
-    canvas.width = div.width
+    canvas = document.getElementById('tutorial');
+    if (canvas.getContext) {
+      ctx = canvas.getContext('2d');
+    }
+    canvas.width = 1600;
+    canvas.height = 900;
     canvas.style.border   = "3px solid";
     ctx = canvas.getContext('2d')
-
-    div.appendChild(canvas)
     startTimer();
     createBomb();
 }
@@ -23,7 +22,11 @@ function createBomb() {
     img.src = "./img/bomb.png"
     ctx.drawImage(img,(canvas.width - img.width)/2 ,(canvas.height-img.height)/2)
     ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 150, 75);
+    ctx.fillRect(0, 0, 150, 75);
+	ctx.moveTo(0,0);
+	ctx.lineTo(canvas.height,canvas.width)
+	ctx.stroke();
+
 }
 function startTimer(){
     clock = setInterval(updateTimer,1000)
@@ -40,6 +43,7 @@ function updateTimer(){
         return;
     }
     timer = parseInt(timer,10)-1;
+
     console.log(timer)
     document.getElementById('bdTime').innerHTML = "Time left: "+timer;
     if(timer < 10){
