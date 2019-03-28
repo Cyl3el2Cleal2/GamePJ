81c53f041965b2e5c4c125c1586be2b6acce7be1
Cyl3el2Cleal2 <nathachian@gmail.com>
2019-03-28 23:09:10 +0700

    Add slide outsite

--- js/game2.js
+++ js/game2.js
@@ -1,57 +1,69 @@
 var canvas;
-var timer=10;
+var timer = 10;
 var clock;
 var ctx;
 function createArea() {
     canvas = document.getElementById('tutorial');
     if (canvas.getContext) {
-      ctx = canvas.getContext('2d');
+        ctx = canvas.getContext('2d');
     }
     canvas.width = 1600;
     canvas.height = 900;
-    canvas.style.border   = "3px solid";
+    canvas.style.border = "3px solid";
     ctx = canvas.getContext('2d')
     startTimer();
     createBomb();
+    slideBar();
+}
+function updateGame(){
+
+}
+function slideBar(){
+    
+    ctx.rect(350, 800, 900, 30)
+    ctx.stroke();
 }
 
 function createBomb() {
 
     ctx = canvas.getContext('2d')
-    var img = document.createElement("IMG")
-    img.src = "./img/bomb.png"
-    ctx.drawImage(img,(canvas.width - img.width)/2 ,(canvas.height-img.height)/2)
+    var img = new Image()
+    img.src = "./img/bomb2.png";
+    img.onload = function () {
+        ctx.drawImage(img, (canvas.width - img.width)/2, 400)
+
+    }
+    ctx.moveTo(canvas.width / 2, 0)
+    ctx.lineTo(canvas.width / 2, canvas.height)
     ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0, 0, 150, 75);
-	ctx.moveTo(0,0);
-	ctx.lineTo(canvas.height,canvas.width)
-	ctx.stroke();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(canvas.width, canvas.height)
+    ctx.stroke();
 
 }
-function startTimer(){
-    clock = setInterval(updateTimer,1000)
+function startTimer() {
+    clock = setInterval(updateTimer, 1000)
 }
 
-function stopTimer(){
+function stopTimer() {
     clearInterval(clock)
 }
 
-function updateTimer(){
-    if(timer == 0)
-    {
+function updateTimer() {
+    if (timer == 0) {
         stopTimer()
         return;
     }
-    timer = parseInt(timer,10)-1;
+    timer = parseInt(timer, 10) - 1;
 
     console.log(timer)
-    document.getElementById('bdTime').innerHTML = "Time left: "+timer;
-    if(timer < 10){
-        if(timer % 2 == 0){
-document.getElementById('bdTime').style.color = "red";
-        }else{
-            
-document.getElementById('bdTime').style.color = "white";
+    document.getElementById('bdTime').innerHTML = "Time left: " + timer;
+    if (timer < 10) {
+        if (timer % 2 == 0) {
+            document.getElementById('bdTime').style.color = "red";
+        } else {
+
+            document.getElementById('bdTime').style.color = "white";
         }
     }
 }
