7f52e627ee93138d6131b05a5394e53b969322f0
Cyl3el2Cleal2 <nathachian@gmail.com>
2019-03-29 03:22:53 +0700

    add keyCode Detecter

--- js/game2.js
+++ js/game2.js
@@ -1,7 +1,22 @@
+
 var canvas;
 var timer = 10;
 var clock;
+var slide;
 var ctx;
+var current = 800;
+
+
+window.addEventListener('keydown', this.check, false);
+
+function check(e) {
+    console.log(e.keyCode)
+    if (parseInt(e.keyCode) >= 48 && parseInt(e.keyCode) <= 57) {
+
+    } else {
+        alert('กดตัวเลขเท่านั้น')
+    }
+}
 function createArea() {
     canvas = document.getElementById('tutorial');
     if (canvas.getContext) {
@@ -11,17 +26,49 @@ function createArea() {
     canvas.height = 900;
     canvas.style.border = "3px solid";
     ctx = canvas.getContext('2d')
+
     startTimer();
     createBomb();
     slideBar();
+    testSegment()
 }
-function updateGame(){
+function testSegment() {
+    ctx.font = '30px sans-serif';
+    ctx.fillText("Hello World", 30, 30)
+}
+function updateGame() {
+    loadSlidebar();
 
 }
-function slideBar(){
-    
+function slideBar() {
+    var img = new Image()
+    img.src = "./img/fire.png"
+    img.onload = function () {
+        ctx.drawImage(img, 150, 750)
+    }
+    var img2 = new Image()
+    img2.src = "./img/lock.png"
+    img2.onload = function () {
+        ctx.drawImage(img2, 1325, 750)
+    }
     ctx.rect(350, 800, 900, 30)
     ctx.stroke();
+}
+function loadSlidebar() {
+    ctx.clearRect(351, 801, current + 4, 28)
+    if (current >= 1250 || current <= 0) {
+        stopTimer()
+    }
+    var grd = ctx.createLinearGradient(0, 0, 1250, 0);
+    grd.addColorStop(0, "red");
+    grd.addColorStop(1, "yellow");
+    // Fill with gradient
+    ctx.fillStyle = grd;
+    ctx.fillRect(351, 801, current, 28);
+    ctx.restore();
+
+    current -= 4;
+
 }
 
 function createBomb() {
@@ -30,7 +77,7 @@ function createBomb() {
     var img = new Image()
     img.src = "./img/bomb2.png";
     img.onload = function () {
-        ctx.drawImage(img, (canvas.width - img.width)/2, 400)
+        ctx.drawImage(img, (canvas.width - img.width) / 2, 400)
 
     }
     ctx.moveTo(canvas.width / 2, 0)
@@ -42,15 +89,17 @@ function createBomb() {
 
 }
 function startTimer() {
+    slide = setInterval(loadSlidebar, 200)
     clock = setInterval(updateTimer, 1000)
 }
 
 function stopTimer() {
     clearInterval(clock)
+    clearInterval(slide)
 }
 
 function updateTimer() {
-    if (timer == 0) {
+    if (timer == 22222) {
         stopTimer()
         return;
     }
