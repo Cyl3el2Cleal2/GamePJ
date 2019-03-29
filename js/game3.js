674648d433436670f83466998146a0f6f6ddd278
PopAps <23138@tkschool.ac.th>
2019-03-29 16:17:20 +0700

    game3 version2

--- js/game3.js
+++ js/game3.js
@@ -2,8 +2,12 @@ var canvas;
 var timer = 20;
 var clock;
 var ctx;
+
+var imgCos;
+var key,pos=0;
 function CreateScene() {
     canvas = document.getElementById('scene');
+
     canvas.width = 1600;
     canvas.height = 900;
 
@@ -13,56 +17,62 @@ function CreateScene() {
     ctx.moveTo(0, 0);
 
 
-    ctx.moveTo(canvas.width * (1 / 3), 0)
-    ctx.lineTo(canvas.width * (1 / 3), canvas.height)
-
-    ctx.moveTo(canvas.width * (2 / 3), 0)
-    ctx.lineTo(canvas.width * (2 / 3), canvas.height)
-
-    ctx.moveTo(0, canvas.height * (1 / 5))
-    ctx.lineTo(canvas.width, canvas.height * (1 / 5))
-    
-    
     ctx.stroke();
     startTimer();
-    createTime();
-    // setBg();
-    
-}
-function custom(){
-    ////Version1///
-    var image = ["./img/bg3.jpg","./img/bg3.jpg","./img/bg3.jpg"];
-    ctx = canvas.getContext('2d')
+    ctx.stroke();
+    custom();
+    startTimer();
 
-    var img = new Image()
-    for(var i = 0;i<image.length;i++){
-        img.src = image[i];
-        // img.onload = function () {  
-        //     ctx.drawImage(img,canvas.width*(2/3),canvas.height*(4/5), 50, 50) 
-        // }
-    }
-   
+    // // setBg();
+
 }
-function setBg(){
+
+function custom() {
+  
+    var image = ["./img/customGame3/W1.png"];
+    ctx = canvas.getContext('2d')
+    var width = canvas.width * (1 / 3) + 200
+    imgCos = new Image()
+    console.log(width)
+    imgCos.src = image[0];
+    imgCos.onload = function () {
+        ctx.drawImage(imgCos, width, canvas.height * (4 / 5) - 80, 200, 200)
+    }
+    document.onkeydown = function (e) {
+        pos = 1;
+        key = window.event ? e.keyCode : e.which;
+    }
+    document.onkeyup = function (e) { pos = 0; }
+    setInterval(function () {
+        
+        if (pos == 0) return;
+        if (key == 37) width -= 10;
+       
+        if (key == 39) width += 10;  
+        canvas.width = canvas.width
+        ctx.drawImage(imgCos, width, canvas.height * (4 / 5) - 100,200, 200);
+    
+    }, 5);
+}
+
+function setBg() {
     ctx = canvas.getContext('2d')
     var img = new Image()
     img.src = "./img/bg3.jpg";
 
     img.onload = function () {
 
-        ctx.drawImage(img,0,0, canvas.width, canvas.height)
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
 
     }
 }
-function MathTitle(){
+function MathTitle() {
 
 }
-function MathAnswer(){
+function MathAnswer() {
 
 }
-function updateMove() {
-        
-}
+
 
 function startTimer() {
     clock = setInterval(updateTimer, 1000)
@@ -72,14 +82,14 @@ function stopTimer() {
     clearInterval(clock)
 }
 function updateTimer() {
-    
+
     if (timer == 0) {
         stopTimer()
         return;
     }
     timer = parseInt(timer, 10) - 1;
 
-    console.log(timer)
+
     document.getElementById('bdTime').innerHTML = "Time : " + timer;
     if (timer < 10) {
         if (timer % 2 == 0) {
