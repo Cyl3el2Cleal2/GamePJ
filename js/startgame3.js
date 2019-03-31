dc4ff1dc261eb123d77afde65650e841ccc625c3
PopAps <23138@tkschool.ac.th>
2019-03-31 18:26:11 +0700

    game3 Version8 + startgame3 + congra

---
+++ js/startgame3.js
@@ -0,0 +1,19 @@
+
+var canvas;
+var ctx;
+var name;
+var data;
+function startgame3() {
+    name = location.search.substring(1)
+    data = name.split("%20")
+    console.log(data[0] + " " +data[1])
+    canvas = document.getElementById('scene');
+    canvas.width = 1600;
+    canvas.height = 900;
+    canvas.style.border = "3px solid";
+    ctx = canvas.getContext('2d')
+    ctx.moveTo(0, 0);
+}
+function nextPage(){
+        window.location.href = "index.html"
+}
